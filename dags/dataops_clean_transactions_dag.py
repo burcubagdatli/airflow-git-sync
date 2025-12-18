6fe0481e9e866e0cf3d9a8fcb83f27fa3b5d1074
burcubagdatli <elfinea.dragonhunter@gmail.com>
2025-12-18 16:05:07 +0300

    Update and rename clean_store_transactions_dag.py to dataops_clean_transactions_dag.py

---
+++ dags/dataops_clean_transactions_dag.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from datetime import datetime
+
+default_args = {
+    "owner": "dataops",
+    "depends_on_past": False,
+}
+
+with DAG(
+    dag_id="dataops_clean_transactions",
+    default_args=default_args,
+    start_date=datetime(2024, 1, 1),
+    schedule=None,          # manuel trigger (full load)
+    catchup=False,
+    tags=["dataops", "ssh", "spark_client"],
+) as dag:
+
+    run_cleaning_job = SSHOperator(
+        task_id="run_clean_transactions",
+        ssh_conn_id="spark_ssh_conn",
+        command="""
+        set -e
+        echo "Downloading CSV from MinIO..."
+
+        wget -O /tmp/dirty_store_transactions.csv \
+        http://minio:9000/dataops-bronze/raw/dirty_store_transactions.csv
+
+        echo "Running data cleaning script..."
+        python /opt/airflow/dags/apps/clean_transactions.py
+        """,
+    )
+
+    run_cleaning_job
